30a5fc61f0d720b4cc91191a3b860c2f28b6e184
kingachwaleba <kingach6@gmail.com>
2021-06-05 22:04:37 +0200

    Implemented SurfaceHolder.Callback interface

--- app/src/main/java/com/example/paint/DrawingSurfaceView.java
+++ app/src/main/java/com/example/paint/DrawingSurfaceView.java
@@ -2,11 +2,29 @@ package com.example.paint;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class DrawingSurfaceView extends SurfaceView {
+import androidx.annotation.NonNull;
+
+public class DrawingSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
     public DrawingSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+
     }
 }
