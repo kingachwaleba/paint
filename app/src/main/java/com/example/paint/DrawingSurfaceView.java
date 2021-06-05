d9e4c923dc8b5cef078b70b98bd8f781ea42d171
kingachwaleba <kingach6@gmail.com>
2021-06-05 22:03:30 +0200

    Inherited the SurfaceView class

--- app/src/main/java/com/example/paint/DrawingSurfaceView.java
+++ app/src/main/java/com/example/paint/DrawingSurfaceView.java
@@ -1,4 +1,12 @@
 package com.example.paint;
 
-public class DrawingSurfaceView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class DrawingSurfaceView extends SurfaceView {
+
+    public DrawingSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
