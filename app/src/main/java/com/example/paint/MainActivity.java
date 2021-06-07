90dc173c711103f1ebae3cb94472e2b87c538373
kingachwaleba <kingach6@gmail.com>
2021-06-07 13:38:35 +0200

    Added references to the view's elements

--- app/src/main/java/com/example/paint/MainActivity.java
+++ app/src/main/java/com/example/paint/MainActivity.java
@@ -3,12 +3,28 @@ package com.example.paint;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button redButton;
+    private Button yellowButton;
+    private Button blueButton;
+    private Button greenButton;
+    private Button clearButton;
+    private DrawingSurfaceView drawingSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        redButton = findViewById(R.id.red_button);
+        yellowButton = findViewById(R.id.yellow_button);
+        blueButton = findViewById(R.id.blue_button);
+        greenButton = findViewById(R.id.green_button);
+        clearButton = findViewById(R.id.clear_all_button);
+
+        drawingSurfaceView = findViewById(R.id.drawing_surface);
     }
 }
